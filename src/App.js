616f3ac454758f0a712e54eafdfa70a8fd63bb61
Aidanwang0309 <aidanwang0309@gmail.com>
2019-08-06 13:31:22 -0400

    Style Appbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">BK TITLE</header>
+      <Home />
     </div>
   );
 }
